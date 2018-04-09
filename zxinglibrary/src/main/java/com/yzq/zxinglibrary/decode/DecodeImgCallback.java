70cb4aebeed8568f0f9482cff479f590b383206a
yzq <1149635502@qq.com>
2018-04-09 14:20:06 +0800

    关闭android.enableAapt2=true

--- zxinglibrary/src/main/java/com/yzq/zxinglibrary/decode/DecodeImgCallback.java
+++ zxinglibrary/src/main/java/com/yzq/zxinglibrary/decode/DecodeImgCallback.java
@@ -4,12 +4,12 @@ import com.google.zxing.Result;
 
 /**
  * Created by yzq on 2017/10/18.
- *
+ * <p>
  * 解析图片的回调
  */
 
 public interface DecodeImgCallback {
-    public void onImageDecodeSuccess(Result result);
+    void onImageDecodeSuccess(Result result);
 
-    public void onImageDecodeFailed();
+    void onImageDecodeFailed();
 }
