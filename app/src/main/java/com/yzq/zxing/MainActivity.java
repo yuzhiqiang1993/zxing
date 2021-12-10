2a422a4f1571dce6f4b90ffeade2fb3a9d92a3e2
XeonYu <zhiqiang.yu.xeon@gmail.com>
2021-12-10 21:38:43 +0800

    update sdkverison and sample code

--- app/src/main/java/com/yzq/zxing/MainActivity.java
+++ app/src/main/java/com/yzq/zxing/MainActivity.java
@@ -17,15 +17,12 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import com.yanzhenjie.permission.Action;
 import com.yanzhenjie.permission.AndPermission;
-import com.yanzhenjie.permission.Permission;
+import com.yanzhenjie.permission.runtime.Permission;
 import com.yzq.zxinglibrary.android.CaptureActivity;
 import com.yzq.zxinglibrary.bean.ZxingConfig;
 import com.yzq.zxinglibrary.common.Constant;
 import com.yzq.zxinglibrary.encode.CodeCreator;
-
-import java.util.List;
 
 
 /**
@@ -104,41 +101,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.scanBtn:
 
                 AndPermission.with(this)
+                        .runtime()
                         .permission(Permission.CAMERA, Permission.READ_EXTERNAL_STORAGE)
-                        .onGranted(new Action() {
-                            @Override
-                            public void onAction(List<String> permissions) {
-                                Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
-                                /*ZxingConfig是配置类
-                                 *可以设置是否显示底部布局，闪光灯，相册，
-                                 * 是否播放提示音  震动
-                                 * 设置扫描框颜色等
-                                 * 也可以不传这个参数
-                                 * */
-                                ZxingConfig config = new ZxingConfig();
-                                // config.setPlayBeep(false);//是否播放扫描声音 默认为true
-                                //  config.setShake(false);//是否震动  默认为true
-                                // config.setDecodeBarCode(false);//是否扫描条形码 默认为true
+                        .onGranted(data -> {
+                            Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+                            /*ZxingConfig是配置类
+                             *可以设置是否显示底部布局，闪光灯，相册，
+                             * 是否播放提示音  震动
+                             * 设置扫描框颜色等
+                             * 也可以不传这个参数
+                             * */
+                            ZxingConfig config = new ZxingConfig();
+                            // config.setPlayBeep(false);//是否播放扫描声音 默认为true
+                            //  config.setShake(false);//是否震动  默认为true
+                            // config.setDecodeBarCode(false);//是否扫描条形码 默认为true
 //                                config.setReactColor(R.color.colorAccent);//设置扫描框四个角的颜色 默认为白色
 //                                config.setFrameLineColor(R.color.colorAccent);//设置扫描框边框颜色 默认无色
 //                                config.setScanLineColor(R.color.colorAccent);//设置扫描线的颜色 默认白色
-                                config.setFullScreenScan(false);//是否全屏扫描  默认为true  设为false则只会在扫描框中扫描
-                                intent.putExtra(Constant.INTENT_ZXING_CONFIG, config);
-                                startActivityForResult(intent, REQUEST_CODE_SCAN);
-                            }
+                            config.setFullScreenScan(false);//是否全屏扫描  默认为true  设为false则只会在扫描框中扫描
+                            intent.putExtra(Constant.INTENT_ZXING_CONFIG, config);
+                            startActivityForResult(intent, REQUEST_CODE_SCAN);
                         })
-                        .onDenied(new Action() {
-                            @Override
-                            public void onAction(List<String> permissions) {
-                                Uri packageURI = Uri.parse("package:" + getPackageName());
-                                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, packageURI);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        .onDenied(data -> {
+                            Uri packageURI = Uri.parse("package:" + getPackageName());
+                            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, packageURI);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-                                startActivity(intent);
+                            startActivity(intent);
 
-                                Toast.makeText(MainActivity.this, "没有权限无法扫描呦", Toast.LENGTH_LONG).show();
-                            }
-                        }).start();
+                            Toast.makeText(MainActivity.this, "没有权限无法扫描呦", Toast.LENGTH_LONG).show();
+                        })
+                        .start();
 
                 break;
             case R.id.encodeBtn:
